f675a1240a046414508bd6c370c638155194d904
Mathias Zinnen~ <mathias.zinnen@fau.de>
2023-03-01 17:13:13 +0100

    finetune head cross eval config

---
+++ myconfs/odor3/head_only_frcnn_r50.py
@@ -0,0 +1,15 @@
+_base_ = [
+    './faster_rcnn_r50_fpn.py',
+    '../../configs/_base_/datasets/odor3_instance.py',
+    './schedule_50e.py', '../../configs/_base_/default_runtime.py'
+]
+
+runner = dict(type='EpochBasedRunner', max_epochs=1)
+
+model = dict(
+    backbone=dict(
+        frozen_stages=4,
+        init_cfg=(type='Pretrained',
+            checkpoint='/net/cluster/zinnen/models/cross-eval/deart_on_odor/epoch_10.pth')
+    )
+)
