621d6a58755d7ad66199898afc97c587f6fb9a14
Mathias <mathias.zinnen@gmail.com>
2023-04-24 14:13:47 +0200

    Add cluster train script

--- cluster/alex/train_alex.sh
+++ cluster/alex/train_alex.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -l
-#SBATCH --time=23:59:59
-#SBATCH --gres=gpu:a100:4
-#SBATCH --job-name=frcnn-swinl
+#SBATCH --time=10:59:59
+#SBATCH --gres=gpu:a40:8
+#SBATCH --job-name=deart_on_odor_head
 #SBATCH --export=NONE
 
 unset SLURM_EXPORT_ENV
 
 module load python
 module load cuda
+
+echo 'execution started'
 
 source activate openmmlab
 
@@ -19,17 +21,23 @@ cp -r /home/woody/iwi5/iwi5064h/mmdetection-ODOR .
 
 cd mmdetection-ODOR
 
+echo 'starting pip install'
+
 pip install .
 
 
 mkdir -p ./data/ODOR-v3
 tar xf /home/janus/iwi5-datasets/odor3/odor3.tar -C ./data/ODOR-v3
 
-GPUS=4
-CONFIG=configs/swin/odor3-swinl.py
-WORK_DIR=/home/woody/iwi5/iwi5064h/mmdetection-workdirs/
 
-./tools/dist_train.sh $CONFIG $GPUS --work-dir $WORK_DIR
+
+GPUS=8
+CONFIG=$1
+WORK_DIR='/home/woody/iwi5/iwi5064h/mmdetection-workdirs/deart_on_odor_head'
+
+echo 'train with ' ${CONFIG}
+
+./tools/dist_train.sh ${CONFIG} ${GPUS}
 
 
 echo "train done"
